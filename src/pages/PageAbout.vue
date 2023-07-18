7628d5d283c76e14240f0645ac4923801e342d61
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2023-07-18 15:32:12 +0500

    chore: designed about page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,10 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus a non, quidem quos, eveniet illo esse alias corrupti rerum numquam ex ea necessitatibus. Inventore sit maiores aperiam laboriosam quod cum!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate numquam quia recusandae deserunt nostrum fuga magnam dolor molestiae! Ipsa veniam atque velit facilis dignissimos hic rem molestias ducimus dicta mollitia!</p>
+    </div>
   </q-page>
 </template>
 
