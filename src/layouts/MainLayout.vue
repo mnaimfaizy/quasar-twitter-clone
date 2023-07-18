847baef3088b31e0ca6a3a4fef38edc60409126d
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2023-07-18 15:07:21 +0500

    chore: designed the main layout chore: designed the left and right drawers chore: designed the header section and logo

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,137 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+
+    <q-header bordered class="bg-white text-black">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title class="text-weight-bold">
+          Qwitter
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+      :width="283"
+    >
+      <q-icon
+        name="fas fa-dove"
+        size="lg"
+        color="primary"
+        class="q-pa-md"
+      />
+      <q-list>
+        <q-item
+          clickable
+          v-ripple
+          to="/"
+        >
+          <q-item-section avatar>
+            <q-icon color="primary" name="home" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+        <q-item
+          clickable
+          v-ripple
+          to="/about"
+        >
+          <q-item-section avatar>
+            <q-icon color="primary" name="help" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-drawer
+      show-if-above
+      v-model="rightDrawerOpen"
+      side="right"
+      bordered
+    >
+      <q-input
+        class="q-ma-md"
+        outlined
+        rounded
+        placeholder="Search Qwitter"
+        dense
+      >
+        <template v-slot:prepared>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <q-list separator padding>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-gray">Education</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happend</q-item-label>
+            <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-gray">Education</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happend</q-item-label>
+            <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-gray">Education</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happend</q-item-label>
+            <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
